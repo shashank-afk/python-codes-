e0877b4820a5e66b97c551778eda20ac040d4630
Amit <52880868+AvTe@users.noreply.github.com>
2022-10-23 12:51:19 +0530

    Python Basie Program

---
+++ 09_Python Variables.py
@@ -0,0 +1,57 @@
+
+
+# >>>>>>>>>>>>>>... https://www.w3schools.com/python/python_variables.asp   <<<<<<<<<<<<<<<
+
+# clik the and check the whole python variable examples and make that !! 
+
+# Variables
+# Variables are containers for storing data values.
+
+# Creating Variables
+# Python has no command for declaring a variable.
+
+# A variable is created the moment you first assign a value to it.
+
+# Example
+x = 5
+y = "Amit"
+print(x)
+print(y)
+# Variables do not need to be declared with any particular type, and can even change type after they have been set.
+
+# Example
+x = 4       # x is of type int
+x = "Siya" # x is now of type str
+print(x)
+# Casting
+# If you want to specify the data type of a variable, this can be done with casting.
+
+# Example
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+# Get the Type
+# You can get the data type of a variable with the type() function.
+
+# Example
+x = 5
+y = "Amit"
+print(type(x))
+print(type(y))
+# You will learn more about data types and casting later in this tutorial.
+# Single or Double Quotes?
+# String variables can be declared either by using single or double quotes:
+
+# Example
+x = "Motu"
+# is the same as
+x = 'Motu'
+# Case-Sensitive
+# Variable names are case-sensitive.
+
+# Example
+# This will create two variables:
+
+a = 4
+A = "Amit"
+#A will not overwrite a
